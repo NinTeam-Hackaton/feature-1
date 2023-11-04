b858a59dea65b591f76379dac434c5fe885d499b
ZaharBerku <zaharberku@gmail.com>
2023-11-04 22:33:30 +0200

    added layout

---
+++ src/utils/helpers/index.ts
@@ -0,0 +1,35 @@
+export const editorButtons = [
+  {
+    id: 0,
+  },
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+];
+
+export const widgets = [
+  {
+    id: 0,
+  },
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+];
