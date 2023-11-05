6485ad8ad4395731c91f8a73fa4ccb49188a919a
Kozachkov Rostyslav <kozachkov.rostislav003@gmail.com>
2023-11-05 03:31:03 +0200

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { LayoutRoot } from "./layout";
 import { SidebarComponent, ComponentsLibrary } from "./components";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <SidebarComponent />
       <ComponentsLibrary />
     </>
